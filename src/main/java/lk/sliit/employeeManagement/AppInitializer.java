1bed8219780bd5171ffdefb38ffd9991929112e4
rivinduchamath <trivinduchamath333@gmail.com>
2020-05-28 22:39:15 +0530

    Add U

--- src/main/java/lk/sliit/employeeManagement/AppInitializer.java
+++ src/main/java/lk/sliit/employeeManagement/AppInitializer.java
@@ -1,5 +1,12 @@
 package lk.sliit.employeeManagement;
-
+/*
+*
+  Created by IntelliJ IDEA.
+  User: Rivindu Chamath
+  Date: 21-May-20
+  Time: 2:43 AM
+  To change this template use File | Settings | File Templates.
+*/
 
 import lk.sliit.employeeManagement.dao.EmployeeDAO;
 import lk.sliit.employeeManagement.entity.Employee;
