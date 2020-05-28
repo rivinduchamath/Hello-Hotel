197fb8ee328b04708a1c31da880b23e552092e35
rivinduchamath <trivinduchamath333@gmail.com>
2020-05-28 22:40:01 +0530

    Add U

--- src/main/java/lk/sliit/employeeManagement/service/SuperBO.java
+++ src/main/java/lk/sliit/employeeManagement/service/SuperBO.java
@@ -1,5 +1,12 @@
 package lk.sliit.employeeManagement.service;
-
+/*
+*
+  Created by IntelliJ IDEA.
+  User: Rivindu Chamath
+  Date: 21-May-20
+  Time: 2:43 AM
+  To change this template use File | Settings | File Templates.
+*/
 
 public interface SuperBO  {
 }
