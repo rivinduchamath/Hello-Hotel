926e53730a8d2a3dd5289a8ed4fcf47a69f7ff7c
Rivindu Wijayarathna <trivinduchamath333@gmail.com>
2020-08-09 15:03:53 +0530

    Add Common Collection

--- src/main/java/lk/sliit/hotelManagement/entity/restaurant/counterOrder/RestaurantCounterOrder.java
+++ src/main/java/lk/sliit/hotelManagement/entity/restaurant/counterOrder/RestaurantCounterOrder.java
@@ -25,7 +25,6 @@ public class RestaurantCounterOrder {
         this.quantity = quantity;
         this.date = date;
         this.customerId = customerId;
-
     }
 
     public RestaurantCounterOrder() {
