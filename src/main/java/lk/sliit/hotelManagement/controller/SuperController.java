6f0f03eda6209c402426eca8c4db63752ee9af5b
Rivindu Wijayarathna <trivinduchamath333@gmail.com>
2020-08-11 00:03:35 +0530

    Inventory Jsp 1

--- src/main/java/lk/sliit/hotelManagement/controller/SuperController.java
+++ src/main/java/lk/sliit/hotelManagement/controller/SuperController.java
@@ -3,4 +3,5 @@ package lk.sliit.hotelManagement.controller;
 
 public class SuperController {  //Super Controller For Store Common Things
     public static String idNo = "1";  //Store Logger User ID
+
 }
