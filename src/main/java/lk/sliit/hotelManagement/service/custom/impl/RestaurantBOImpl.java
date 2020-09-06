a48f61e4787dd21b81db8b222f518f968a34df26
Rivindu Wijayarathna <trivinduchamath333@gmail.com>
2020-09-06 13:31:40 +0530

    Add Retaurant

---
+++ src/main/java/lk/sliit/hotelManagement/service/custom/impl/RestaurantBOImpl.java
@@ -0,0 +1,21 @@
+package lk.sliit.hotelManagement.service.custom.impl;
+
+import lk.sliit.hotelManagement.service.custom.RestaurantBO;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class RestaurantBOImpl implements RestaurantBO {
+
+//    Online table
+
+
+    //Onlene Order
+
+
+    //Counter Table
+
+
+    //Counter Order
+}
