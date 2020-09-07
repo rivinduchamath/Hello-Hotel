01fa5a091560af016517fa8e6733851296b68271
AmarangiKarunathilaka <amarangikarunathilaka@gmail.com>
2020-09-07 15:38:59 +0530

    online sign in

--- src/main/java/lk/sliit/hotelManagement/controller/foodAndBevarage/Restaurant/OnlineDashboard.java
+++ src/main/java/lk/sliit/hotelManagement/controller/foodAndBevarage/Restaurant/OnlineDashboard.java
@@ -1,31 +1,22 @@
 package lk.sliit.hotelManagement.controller.foodAndBevarage.Restaurant;
 
 import lk.sliit.hotelManagement.controller.SuperController;
-import lk.sliit.hotelManagement.dto.manager.EmployeeDTO;
-import lk.sliit.hotelManagement.dto.manager.NoticeDTO;
-import lk.sliit.hotelManagement.dto.restaurant.OnlineCustomerDTO;
 import lk.sliit.hotelManagement.service.custom.IndexLoginBO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 @Controller
 public class OnlineDashboard {
     @Autowired
     IndexLoginBO indexLoginBO;
     @RequestMapping("/onlineDashboard")
-    public String registerUser(@ModelAttribute OnlineCustomerDTO onlineCustomer, Model model) {
-        //True If Id and password is match
-        if (indexLoginBO.findByUserNameAndPassword(onlineCustomer.getUserName(), onlineCustomer.getPassword()) != null) {
-            return "/onlineDashboard";
-        } else {//If User name And Password is not match
-            return "redirect:/onlineCustomer";
-        }
+    public String loadForm_validationSaveMode(Model model, HttpServletRequest request) {
+        model.addAttribute("loggerName", indexLoginBO.getEmployeeByIdNo(SuperController.idNo));
+        return "onlineDashboard";
     }
 }
