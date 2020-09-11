13e46fc7892d114d7d70b5d2cebc3161354310bc
Rivindu Wijayarathna <trivinduchamath333@gmail.com>
2020-09-11 14:38:41 +0530

    Add int to Reservation

--- src/main/java/lk/sliit/hotelManagement/dao/beverageDAO/BarOrdersDAO.java
+++ src/main/java/lk/sliit/hotelManagement/dao/beverageDAO/BarOrdersDAO.java
@@ -4,7 +4,7 @@ import lk.sliit.hotelManagement.entity.barManage.BarOrders;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface BarOrdersDAO extends CrudRepository<BarOrders,String> {
+public interface BarOrdersDAO extends CrudRepository<BarOrders,Integer> {
     BarOrders findTopByOrderByOrderIdDesc();
 
 }
