fcbe93ec612e0839f91988f621256e3ac22f605e
Rivindu Wijayarathna <trivinduchamath333@gmail.com>
2020-10-28 07:43:42 +0530

    Add comments

--- src/main/java/lk/sliit/hotelManagement/controller/foodAndBevarage/Restaurant/RestaurantTableController.java
+++ src/main/java/lk/sliit/hotelManagement/controller/foodAndBevarage/Restaurant/RestaurantTableController.java
@@ -29,7 +29,7 @@ public class RestaurantTableController {
     @Autowired
     RestaurantBO restaurantBO;
 
-    @GetMapping("/restaurantTable")
+    @GetMapping("/restaurantTable")//Load Laundry Table
     public String loginPage(Model model) {
         model.addAttribute("loggerName", indexLoginBO.getEmployeeByIdNo(SuperController.idNo));
         List<RestaurantTableDTO> tableList = restaurantBO.findAllTable();
@@ -40,30 +40,30 @@ public class RestaurantTableController {
     @PostMapping("/saveTable")
     public String addNewTable(Model model, @ModelAttribute RestaurantTableDTO restaurantTableDTO) {
          try {
-            RestaurantTableDTO tableDTO1 = restaurantBO.findHighestTableId();
+            RestaurantTableDTO tableDTO1 = restaurantBO.findHighestTableId();//Find highest Id
             RestaurantTableDTO tableDTO2 = null;
             try {
-                tableDTO2 = restaurantBO.findTableById(restaurantTableDTO.getTableId());
+                tableDTO2 = restaurantBO.findTableById(restaurantTableDTO.getTableId());//find One
             } catch (NullPointerException d) {
                 int maxId = (tableDTO1.getTableId());
-                if (restaurantTableDTO.getTableId() == (maxId)) {
+                if (restaurantTableDTO.getTableId() == (maxId)) {//Update
                     restaurantTableDTO.setTableId((maxId));
-                } else {
+                } else {//New Table
                     maxId++;
                     restaurantTableDTO.setTableId((maxId));
                 }
             }
 
-        } catch (NullPointerException e) {
+        } catch (NullPointerException e) {//Initial Round
             restaurantTableDTO.setTableId(1);
         }
 
         model.addAttribute("loggerName", indexLoginBO.getEmployeeByIdNo(SuperController.idNo));
-        restaurantBO.saveTable(restaurantTableDTO);
+        restaurantBO.saveTable(restaurantTableDTO);//Save Table
         return "redirect:/restaurantTable";
     }
 
-    @GetMapping(value = "deleteTable/{tableId}")
+    @GetMapping(value = "deleteTable/{tableId}")//Delete Table
     public void deleteTable(@PathVariable("tableId") int tableId, HttpServletResponse response) {
         restaurantBO.deleteTable(tableId);
         try {
@@ -74,7 +74,7 @@ public class RestaurantTableController {
     }
 
     @GetMapping("/restaurantTableIndex")
-    public String restaurantTableIndex(Model model) {
+    public String restaurantTableIndex(Model model) {//Find Today Booked Tables
         model.addAttribute("loggerName", indexLoginBO.getEmployeeByIdNo(SuperController.idNo));
         List<CounterTableReservationDTO> p2 = restaurantBO.getBookedTables();
         model.addAttribute("todayBookedTables", p2);
@@ -96,38 +96,39 @@ public class RestaurantTableController {
         Time a2 = Time.valueOf(counterTableReservationDTO.getvEndT()+":00");
         counterTableReservationDTO.setStartTime(a);//Time set to dto
         counterTableReservationDTO.setEndTime(a2);
-        Date date = Date.valueOf(counterTableReservationDTO.getvDate());
+        Date date = Date.valueOf(counterTableReservationDTO.getvDate());//Set Date
         counterTableReservationDTO.setDate(date);
-        model.addAttribute("reservedDate", (counterTableReservationDTO.getDate()));
+        model.addAttribute("reservedDate", (counterTableReservationDTO.getDate()));//Set Values to next Page
         model.addAttribute("timeIn", (counterTableReservationDTO.getStartTime()));
         model.addAttribute("timeOut", (counterTableReservationDTO.getEndTime()));
-        List<RestaurantTableDTO> p2 =restaurantBO.getAviTables(counterTableReservationDTO.getDate(),
+        List<RestaurantTableDTO> p2 =restaurantBO.getAviTables(counterTableReservationDTO.getDate(),//Find available
                 counterTableReservationDTO.getStartTime(),counterTableReservationDTO.getEndTime());
         model.addAttribute("loadAllTable", p2);
         return "counterTableDetails";
     }
 
-    @PostMapping("/saveCounterTable")
+    @PostMapping("/saveCounterTable")//SAve Table in counter Table Reservations
     public String saveOnlineTable(@ModelAttribute CounterTableReservationDTO onlineOrderDTO, HttpSession session) {
 
         try {
-            Time a = Time.valueOf(onlineOrderDTO.getvStatT());
+            Time a = Time.valueOf(onlineOrderDTO.getvStatT());//Get time and
             Time a2 = Time.valueOf(onlineOrderDTO.getvEndT());
-            onlineOrderDTO.setStartTime(a);
+            onlineOrderDTO.setStartTime(a);// set time to time type
             onlineOrderDTO.setEndTime(a2);
             Date date = Date.valueOf(onlineOrderDTO.getvDate());
             onlineOrderDTO.setDate(date);
         }catch (IllegalArgumentException s){}
         try {
+            //find highest Id
              CounterTableReservationDTO top = restaurantBO.findHighestCounterTableId();
             int x = (top.getCounterTableReserveId()) + 1;
             onlineOrderDTO.setCounterTableReserveId((x));
-        } catch (NullPointerException e) {
-            System.out.println("In Try Catch");
+        } catch (NullPointerException e) {//If not found any Id
+
             onlineOrderDTO.setCounterTableReserveId((1));
         }
         try {
-             restaurantBO.saveCounterTableId(onlineOrderDTO);
+             restaurantBO.saveCounterTableId(onlineOrderDTO);//SAve
         } catch (NullPointerException d) {
             return "redirect:/restaurantTableReservation";
         }
