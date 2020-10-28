3aae7ab1c7b5baea076c4eba511bbb992809d72b
BuddhiPerera <buddhiperera27@gmail.com>
2020-10-28 16:14:20 +0530

    add comments

--- src/main/java/lk/sliit/hotelManagement/controller/humanResourceController/currentBillController/CurrentBillController.java
+++ src/main/java/lk/sliit/hotelManagement/controller/humanResourceController/currentBillController/CurrentBillController.java
@@ -20,7 +20,7 @@ import java.util.List;
 
 @Controller
 public class CurrentBillController {
-
+    // automate the object creation and connect with the relevant interfaces
     @Autowired
     IndexLoginBO indexLoginBO;
 
@@ -30,48 +30,47 @@ public class CurrentBillController {
     @Autowired
     CurrentBO currentBO;
 
-    @GetMapping ("/currentBill")//load Curernt bill page
+    @GetMapping ("/currentBill")//load Current bill page and data
     public ModelAndView loadCurrentBillDetails(Model model) {
         ModelAndView modelAndView = new ModelAndView("currentBill");
         model.addAttribute("loggerName", indexLoginBO.getEmployeeByIdNo(SuperController.idNo));
-        List<CurrentBillDTO> cbDTOList = currentBO.findAllCurrentBills();
+        List<CurrentBillDTO> cbDTOList = currentBO.findAllCurrentBills(); // find available data
         modelAndView.addObject("loadCBTable", cbDTOList);
         return modelAndView;
     }
 
-    @PostMapping("saveCurrentBill")//save Current
+    @PostMapping("saveCurrentBill")//save Current bill
     public String saveCurrentBillForm(@ModelAttribute CurrentBillDTO currentBillDTO){
         try {
 
-            CurrentBillDTO current = currentBO.findHighestCurrentBillId();
-            CurrentBillDTO currentBillDTO1 = null;
+            CurrentBillDTO current = currentBO.findHighestCurrentBillId(); // get highest bill id
+            CurrentBillDTO currentBillDTO1 = null; //set dto object to null
             try {
-                currentBillDTO1 = currentBO.findById(currentBillDTO.getBillId());
+                currentBillDTO1 = currentBO.findById(currentBillDTO.getBillId()); // get the next bill id
 
             }catch (NullPointerException d){
-                int maxId = (current.getBillId());
-                if (currentBillDTO.getBillId()==(maxId)) {//Update Bill Id
+                int maxId = (current.getBillId()); // get available bill id
+
+                if (currentBillDTO.getBillId()==(maxId)) {//If true Update Bill Id
                     currentBillDTO.setBillId((maxId));
-                } else {//New Current bill
+                } else {//create new Current bill
                     maxId++;
                     currentBillDTO.setBillId((maxId));
                 }
             }
 
         } catch (NullPointerException e){
-            currentBillDTO.setBillId(1);
+            currentBillDTO.setBillId(1); // set bill id as 1
         }
         currentBO.saveCurrentBill(currentBillDTO);//save
         return "redirect:/currentBill";
     }
 
+    // delete current bill
     @RequestMapping(value = "deleteCurrentBill/{billId}")
     public void deleteEmployee(@PathVariable("billId") int billId, HttpServletResponse response) throws IOException {
-
             currentBO.deleteCurrentBill(billId);
             response.sendRedirect("/currentBill");
-
-
     }
 
 }
